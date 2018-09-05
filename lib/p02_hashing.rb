72422775d6692bf539820fc2482bebc6537a0f95
Jordan Yu <jordanyu44@gmail.com>
2018-09-05 11:32:32 -0700

    Monkey patch hash methods for array, string, hash

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -1,14 +1,25 @@
 class Fixnum
-  # Fixnum#hash already implemented for you
+  # Fixnum#hash already implemented 
 end
 
 class Array
   def hash
+    hashed = ''
+    return 100100100 if self.empty?
+    self.each_with_index do |el, idx|
+      hashed += idx.to_s + el.hash.to_s
+    end
+    hashed.to_i
   end
 end
 
 class String
   def hash
+    hashed = ''
+    self.chars.each do |char|
+      hashed += (char.ord % 21).to_s
+    end
+    return hashed.to_i 
   end
 end
 
@@ -16,6 +27,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hashed = ''
+    self.keys.sort.each do |key|
+      hashed += (key.hash * self[key].hash).to_s
+    end
+    return hashed.to_i
   end
 end
