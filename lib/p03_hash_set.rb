3d8f9e2fa66973052752b77018c873597bc48c55
Jordan Yu <jordanyu44@gmail.com>
2018-09-05 11:43:49 -0700

    Build HashSet class to functional level

--- lib/p03_hash_set.rb
+++ lib/p03_hash_set.rb
@@ -9,18 +9,26 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count == num_buckets
+    self[key] << key
+    self.count = count + 1
   end
 
   def include?(key)
+    !self[key].empty?
   end
 
   def remove(key)
+    @store[(key - 1) % num_buckets] = []
+    self.count = count - 1
   end
 
   private
 
+  attr_writer :count
+
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[(num - 1) % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +36,10 @@ class HashSet
   end
 
   def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |arr| 
+      new_store[(arr.first - 1) % new_store.length] << arr.first
+    end
+    @store = new_store
   end
 end
