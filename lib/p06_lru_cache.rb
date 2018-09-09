01788c22717d221228c3546930519c39224394dd
Jordan Yu <jordanyu44@gmail.com>
2018-09-09 15:29:29 -0700

    Build LRUCache class

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -15,22 +15,40 @@ class LRUCache
   end
 
   def get(key)
+    if map.include?(key)
+      update_node!(map[key])
+    else
+      eject! if count == max 
+      self.store.append(key, calc!(key))
+      self.map[key] = store.last 
+    end
+    store.last.val
   end
 
   def to_s
     "Map: " + @map.to_s + "\n" + "Store: " + @store.to_s
   end
 
+  protected 
+
+  attr_accessor :map, :store
+
   private
 
+  attr_reader :prc, :max
+
   def calc!(key)
-    # suggested helper method; insert an (un-cached) key
+    prc.call(key)
   end
 
   def update_node!(node)
-    # suggested helper method; move a node to the end of the list
+    self.store.remove(node.key)
+    self.store.append(node.key, node.val)
   end
 
   def eject!
+    ejected_key = store.first.key 
+    store.remove(ejected_key)
+    self.map.delete(ejected_key)
   end
 end
