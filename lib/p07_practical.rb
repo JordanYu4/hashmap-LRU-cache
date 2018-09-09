229dcd8777317dea1e15c2c7acd38cbfb4e3d410
Jordan Yu <jordanyu44@gmail.com>
2018-09-09 15:47:05 -0700

    Complete palindrome tester using hash map

--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,18 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string) 
-  
+  char_counts = HashMap.new
+  string.chars.each do |char| 
+    if char_counts.include?(char)
+      char_counts[char] += 1
+    else
+      char_counts[char] = 1
+    end
+  end
+  odd_chars = 0 
+  char_counts.each do |char|
+    odd_chars += 1 if char[1].odd? 
+    return false if odd_chars > 1
+  end
+  true
 end
